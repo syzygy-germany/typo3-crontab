1192ed868fb9ad336b500db181c772182f868d69
Manuel Selbach <m.selbach@syzygy.de>
2020-09-17 14:07:41 +0200

    [TASK] Read also the "old" extension configuration

--- ext_tables.php
+++ ext_tables.php
@@ -15,6 +15,11 @@
             'labels' => 'LLL:EXT:crontab/Resources/Private/Language/locallang_mod.xlf'
         ]
     );
+    // this is only relevant for TYPO3 8.7
+    if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['crontab'])) {
+        $extensionConfiguration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['crontab'], [false]);
+        $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['crontab']['hideSchedulerModule'] = $extensionConfiguration['hideSchedulerModule'];
+    }
     if (!empty($GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['crontab']['hideSchedulerModule'])) {
         $GLOBALS['TBE_MODULES']['system'] = str_replace([',txschedulerM1', ',,'], ['',','], $GLOBALS['TBE_MODULES']['system']);
     }
